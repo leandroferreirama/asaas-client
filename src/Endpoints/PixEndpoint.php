db3a1e1023e9d1688ec29fe0e1b2343a3b7cc079
Leandro Ferreira Marcelli <suporte@integracaosistema.com.br>
2025-03-22 13:02:38 -0300

    Implementação de transferência e pagamento pix.

---
+++ src/Endpoints/PixEndpoint.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Asaas\Endpoints;
+
+use Asaas\Contracts\HttpClientInterface;
+use Asaas\Models\BaseCharge;
+
+class PixEndpoint
+{
+    private HttpClientInterface $httpClient;
+
+    public function __construct(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    public function createWithQrCode(BaseCharge $pixCharge): array
+    {
+        // Etapa 1: Criação da cobrança PIX
+        $chargeResponse = $this->httpClient->request('POST', '/lean/payments', $pixCharge->toArray());
+
+        // Verificando se o ID foi retornado na chave 'response'
+        if (isset($chargeResponse['response']['id'])) {
+            $paymentId = $chargeResponse['response']['id'];
+
+            // Etapa 2: Recuperação do QR Code do PIX
+            $qrCodeResponse = $this->httpClient->request('GET', "/payments/{$paymentId}/pixQrCode");
+
+            // Adicionando o QR Code à resposta original
+            $chargeResponse['response']['pixQrCode'] = $qrCodeResponse['response'];
+        } else {
+            throw new \Exception('Erro ao criar a cobrança PIX: ID não retornado na resposta.');
+        }
+
+        return $chargeResponse;
+    }
+}
