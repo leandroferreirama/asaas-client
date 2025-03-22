2c13a54098e3b74734308c29f24f06c224020e6e
Leandro Ferreira Marcelli <suporte@integracaosistema.com.br>
2025-03-22 12:58:44 -0300

    Implementação de transferência e pagamento pix.

--- example/create_payment.php
+++ example/create_payment.php
@@ -24,8 +24,7 @@ try {
 
     // Criar pagamento de conta479201012298800000030400
     $payment = new Payment(
-        '03399.03718 94400.000009 15479.201012 2 98800000030400',
-        '2024-11-25'
+        '00190.00009 02831.814005 21320.604172 7 10370000012830',
     );
 
     $response = $paymentEndpoint->create($payment);
